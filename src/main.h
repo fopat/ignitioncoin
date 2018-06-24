30bc3f7279684ea963b4b3cca9f14d9f0d98248f
Jayray <jeremy.richert1@gmail.com>
2018-06-25 00:37:10 +0200

    Removed other fixes
    Removed fixes that were not for NeoScrypt or OSS

--- src/main.h
+++ src/main.h
@@ -74,10 +74,7 @@ inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MO
 /** Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp. */
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 
-static const uint BLOCK_LIMITER_TIME = 240;
-
 static const int64_t DRIFT = 120;
-inline int64_t PastDrift(int64_t nTime) { return nTime - DRIFT; }
 inline int64_t FutureDrift(int64_t nTime) { return nTime + DRIFT; }
 
 /** "reject" message codes **/
